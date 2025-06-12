10d85b406882b839f7ad3d7f16c57864e518a29f
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 21:42:12 +0530

    Update orderbook.py

--- orderbook.py
+++ orderbook.py
@@ -1,9 +1,20 @@
 import streamlit as st
 import pandas as pd
-from utils import definedge_get
+from utils import integrate_get
 
 def show():
-    st.header("📚 Order Book")
-    data = definedge_get("/orders")
-    df = pd.DataFrame(data.get("orders", []))
-    st.dataframe(df)
+    st.header("Order Book & Trade Book")
+    st.subheader("Order Book")
+    data = integrate_get("/orders")
+    orders = data.get("orders", [])
+    if orders:
+        st.dataframe(pd.DataFrame(orders))
+    else:
+        st.info("No orders found.")
+    st.subheader("Trade Book")
+    data = integrate_get("/trades")
+    trades = data.get("trades", [])
+    if trades:
+        st.dataframe(pd.DataFrame(trades))
+    else:
+        st.info("No trades found.")
