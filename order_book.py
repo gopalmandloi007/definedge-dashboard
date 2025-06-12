65cc831e795976356f41815e1382fe5232034ea6
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 21:40:55 +0530

    Delete order_book.py

--- order_book.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-from utils import definedge_get
-
-def show():
-    st.header("📚 Order Book")
-    data = definedge_get("/orders")
-    df = pd.DataFrame(data.get("orders", []))
-    st.dataframe(df)
