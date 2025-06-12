8c50eeca0aa54bab12a491870ff3868c9dd2aad9
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 20:55:03 +0530

    Update margin.py

--- margin.py
+++ margin.py
@@ -1,1 +1,21 @@
+import streamlit as st
+from utils import definedge_get, definedge_post
 
+def show():
+    st.header("🔢 Margins & Span")
+    st.subheader("Get Margin for Orders")
+    # You can implement basket margin requests here
+
+    st.subheader("Span Calculator")
+    with st.form("span_calc"):
+        positions = st.text_area("Positions JSON (as list)", value='[{}]')
+        submit = st.form_submit_button("Calculate Span")
+        if submit:
+            try:
+                import json
+                pos_list = json.loads(positions)
+                data = {"positions": pos_list}
+                resp = definedge_post("/spancalculator", data)
+                st.write(resp)
+            except Exception as e:
+                st.error(str(e))
