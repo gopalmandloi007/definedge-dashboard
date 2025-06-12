dcc5609d683bd9fdb7d9e08b8d41c1a78b44b769
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 21:42:53 +0530

    Update limits.py

--- limits.py
+++ limits.py
@@ -1,31 +1,31 @@
 import streamlit as st
-from utils import definedge_get, definedge_post
+from utils import integrate_get, integrate_post
 
 def show():
-    st.header("💸 Limits / Product Conversion")
+    st.header("Limits & Product Conversion")
     st.subheader("Limits")
-    data = definedge_get("/limits")
+    data = integrate_get("/limits")
     st.json(data)
 
     st.subheader("Product Conversion")
     with st.form("prod_conv"):
-        exch = st.selectbox("Exchange", ["NSE", "NFO", "BSE", "MCX"])
         tradingsymbol = st.text_input("Trading Symbol")
-        qty = st.number_input("Quantity", step=1)
-        prev_prod = st.selectbox("Previous Product", ["CNC", "INTRADAY", "NORMAL"])
-        new_prod = st.selectbox("New Product", ["CNC", "INTRADAY", "NORMAL"])
+        exchange = st.selectbox("Exchange", ["NSE", "NFO", "BSE", "MCX"])
+        quantity = st.number_input("Quantity", step=1)
+        previous_product = st.selectbox("Previous Product", ["CNC", "INTRADAY", "NORMAL"])
+        new_product = st.selectbox("New Product", ["CNC", "INTRADAY", "NORMAL"])
         order_type = st.selectbox("Order Type", ["BUY", "SELL"])
-        pos_type = st.text_input("Position Type", value="DAY")
+        position_type = st.selectbox("Position Type", ["DAY", "CARRYFORWARD"])
         submit = st.form_submit_button("Convert Product")
         if submit:
             data = {
-                "exchange": exch,
                 "tradingsymbol": tradingsymbol,
-                "quantity": qty,
-                "previous_product": prev_prod,
+                "exchange": exchange,
+                "quantity": quantity,
+                "previous_product": previous_product,
+                "product_type": new_product,
                 "order_type": order_type,
-                "product_type": new_prod,
-                "position_type": pos_type,
+                "position_type": position_type
             }
-            resp = definedge_post("/productconversion", data)
+            resp = integrate_post("/positions/convert", data)
             st.write(resp)
