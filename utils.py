11219e7f722a3f04d289fd2b02e290f294427e4a
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 19:10:10 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from pyintegrate import ConnectToIntegrate, IntegrateOrders
 
+@st.cache_resource(show_spinner=False)
+def get_integrate_client():
+    # Set these as Streamlit secrets!
+    api_token = st.secrets["integrate_api_token"]
+    api_secret = st.secrets["integrate_api_secret"]
+    uid = st.secrets["integrate_uid"]
+    actid = st.secrets["integrate_actid"]
+    api_session_key = st.secrets["integrate_api_session_key"]
+    ws_session_key = st.secrets["integrate_ws_session_key"]
+
+    conn = ConnectToIntegrate()
+    conn.login(api_token, api_secret)
+    conn.set_session_keys(uid, actid, api_session_key, ws_session_key)
+    io = IntegrateOrders(conn)
+    return conn, io
