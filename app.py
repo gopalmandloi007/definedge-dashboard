ce961105a9e56fe5ce2c497320b654defa8fcfce
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 20:23:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ st.title("Definedge Dashboard")
 
 PAGES = {
     "Holdings/Positions": "holdings",
-    "Order Book": "order_book",
-    "Place Order": "place_order",
-    "GTT Orders": "gtt"
+    # Add other modules as needed
 }
 
 page = st.sidebar.selectbox("Go to", list(PAGES.keys()))
@@ -15,12 +13,4 @@ page = st.sidebar.selectbox("Go to", list(PAGES.keys()))
 if page == "Holdings/Positions":
     import holdings
     holdings.show()
-elif page == "Order Book":
-    import order_book
-    order_book.show()
-elif page == "Place Order":
-    import place_order
-    place_order.show()
-elif page == "GTT Orders":
-    import gtt
-    gtt.show()
+# Add more elif blocks for other modules
