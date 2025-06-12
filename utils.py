658e259d34b50bd47314edd8a2b25bb66c912ebb
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 20:16:15 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,16 +3,13 @@ from integrate import ConnectToIntegrate, IntegrateOrders
 
 @st.cache_resource(show_spinner=False)
 def get_integrate_client():
-    # Set these as Streamlit secrets!
-    api_token = st.secrets["integrate_api_token"]
-    api_secret = st.secrets["integrate_api_secret"]
+    # Only use session keys, do not call login() if they are valid!
     uid = st.secrets["integrate_uid"]
     actid = st.secrets["integrate_actid"]
     api_session_key = st.secrets["integrate_api_session_key"]
     ws_session_key = st.secrets["integrate_ws_session_key"]
 
     conn = ConnectToIntegrate()
-    conn.login(api_token, api_secret)
     conn.set_session_keys(uid, actid, api_session_key, ws_session_key)
     io = IntegrateOrders(conn)
     return conn, io
