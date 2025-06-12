f3bfad16cd41241a8821a7ec220aedc726bfec46
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 21:59:29 +0530

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -1,13 +1,13 @@
 import streamlit as st
-from utils import definedge_get
+from utils import integrate_get
 
 def show():
-    st.header("💹 Quotes & Security Info")
-    exch = st.text_input("Exchange", value="NSE")
+    st.header("Get Quotes / Security Info")
+    exchange = st.text_input("Exchange", value="NSE")
     token = st.text_input("Token", value="22")
     if st.button("Get Quotes"):
-        data = definedge_get(f"/quotes/{exch}/{token}")
+        data = integrate_get(f"/quotes/{exchange}/{token}")
         st.json(data)
     if st.button("Get Security Info"):
-        data = definedge_get(f"/securityinfo/{exch}/{token}")
+        data = integrate_get(f"/securityinfo/{exchange}/{token}")
         st.json(data)
