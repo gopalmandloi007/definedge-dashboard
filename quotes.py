77e63b766d6d1f03dc85d09db4a6b8994bf27b1d
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 20:55:24 +0530

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from utils import definedge_get
 
+def show():
+    st.header("💹 Quotes & Security Info")
+    exch = st.text_input("Exchange", value="NSE")
+    token = st.text_input("Token", value="22")
+    if st.button("Get Quotes"):
+        data = definedge_get(f"/quotes/{exch}/{token}")
+        st.json(data)
+    if st.button("Get Security Info"):
+        data = definedge_get(f"/securityinfo/{exch}/{token}")
+        st.json(data)
