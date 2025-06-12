60475a0c81f6b34d248a6eb0f1543c4c0559992f
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 23:23:59 +0530

    Update __init__.py

--- integrate/__init__.py
+++ integrate/__init__.py
@@ -1,1 +1,11 @@
+class ConnectToIntegrate:
+    def login(self, *args, **kwargs):
+        pass
+    def set_session_keys(self, *args, **kwargs):
+        pass
 
+class IntegrateOrders:
+    def __init__(self, conn):
+        self.conn = conn
+    def holdings(self):
+        return {"data": []}  # Dummy data, real logic baad me
