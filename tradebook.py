94dcfb0bbd8b4635700e95f649471a5cd7da84e8
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-12 20:44:52 +0530

    Update tradebook.py

--- tradebook.py
+++ tradebook.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
+from utils import definedge_get
 
+def show():
+    st.header("📖 Trade Book")
+    data = definedge_get("/trades")
+    df = pd.DataFrame(data.get("trades", []))
+    st.dataframe(df)
