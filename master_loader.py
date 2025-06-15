a7c3aaa1c7d27fa6f457dc62c05062764792c522
gopalmandloi007 <gopalmandloi007@gmail.com>
2025-06-15 11:52:40 +0530

    Update master_loader.py

--- master_loader.py
+++ master_loader.py
@@ -1,14 +1,13 @@
 import pandas as pd
 
 def load_watchlist(filename):
-    # Read the file, which now always has 15 columns
+    # All files now have 15 columns; last column ("company") is blank for indices
     columns = [
         "segment", "token", "symbol", "instrument", "series", "isin1",
-        "facevalue", "lot", "something", "zero1", "two1", "one1", "isin", "one2", "company"
+        "facevalue", "lot", "something", "zero1", "two1", "one1", "isin", "company"
     ]
     df = pd.read_csv(filename, sep="\t", header=None, dtype=str)
-    # Assign the first 15 columns only (if extra columns, ignore; if less, will error, which is correct for your standard)
-    df = df.iloc[:, :15]
+    df = df.iloc[:, :15]  # Always trim to 15 columns in case of extra
     df.columns = columns
     return df[["segment", "token", "symbol", "instrument", "company"]]
 
